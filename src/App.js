1a1926de8cfd911f6e346f6e259713937fd949f0
AB <amarbat@gmail.com>
2022-01-29 19:29:12 -0500

    Use Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
+import {Routes, Route, Link} from "react-router-dom";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+
+import Landing from "./components/Landing";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="header">
-        <Header/>
+
+      <div className="nav-bar">
+        <div className="nav-logo">
+          <Link to="/">Logo</Link>
+        </div>
+        <div className="nav-menu">
+          <div className="nav-menu-item">
+            <Link to="/auth/login">Login</Link>
+          </div>
+          <div className="nav-menu-item">
+            <Link to="/auth/signup">SignUp</Link>
+          </div>
+        </div>
       </div>
+
       <div className="body">
-        <Body/>
+        <Routes>
+          <Route exact path="/" element={<Landing/>}/>
+          <Route exact path="/auth/login" element={<Login/>}/>
+          <Route exact path="/auth/signup" element={<SignUp/>}/>
+        </Routes>
       </div>
+
       <div className="footer">
-        <Footer/>
+        Footer
       </div>
+
     </div>
   );
 }
---
+++ src/components/auth/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className="Header">
+      <div className="nav-logo">Login Route</div>
+    </div>
+  );
+}
+
+export default Login;
